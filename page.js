e5d798f9d6db3fda80580226a0e5328d75444cd3
Gabriela da Rosa <rosadgabriela@gmail.com>
2023-07-14 11:09:58 -0300

    projeto final

--- page.js
+++ page.js
@@ -6,14 +6,60 @@ c.height = window.innerHeight;
 c.width = window.innerWidth;
 
 const letters = [
-    "日","ﾊ","ﾐ","ﾋ","ｰ","ｳ","ｼ","ﾅ","ﾓ","ﾆ","ｻ","ﾜ","ﾂ","ｵ","ﾘ","ｱ","ﾎ","ﾃ","ﾏ","ｹ","ﾒ","ｴ","ｶ","ｷ","ﾑ","ﾕ","ﾗ","ｾ","ﾈ","ｽ","ﾀ","ﾇ","ﾍ",":","・",".","=","*","+","-","<",">","¦","｜","ﾘ"
+    "日",
+    "ﾊ",
+    "ﾐ",
+    "ﾋ",
+    "ｰ",
+    "ｳ",
+    "ｼ",
+    "ﾅ",
+    "ﾓ",
+    "ﾆ",
+    "ｻ",
+    "ﾜ",
+    "ﾂ",
+    "ｵ",
+    "ﾘ",
+    "ｱ",
+    "ﾎ",
+    "ﾃ",
+    "ﾏ",
+    "ｹ",
+    "ﾒ",
+    "ｴ",
+    "ｶ",
+    "ｷ",
+    "ﾑ",
+    "ﾕ",
+    "ﾗ",
+    "ｾ",
+    "ﾈ",
+    "ｽ",
+    "ﾀ",
+    "ﾇ",
+    "ﾍ",
+    ":",
+    "・",
+    ".",
+    "=",
+    "*",
+    "+",
+    "-",
+    "<",
+    ">",
+    "¦",
+    "｜",
+    "ﾘ",
 ];
 
 const fontSize = 18;
 
 const columns = c.width / fontSize;
 
-const drops = new Array(Math.floor(columns)).fill(1)
+let drops = [];
+
+for (let x = 0; x < columns; x++) drops[x] = 1;
 
 function draw() {
     ctx.fillStyle = "rgba(0, 0, 0, 0.05)";
@@ -24,11 +70,19 @@ function draw() {
     
     for (let i = 0; i < drops.length; i++) {
         const text = letters[Math.floor(Math.random() * letters.length)];
+
         ctx.fillText(text, i * fontSize, drops[i] * fontSize);
+
+        if (drops[i] * fontSize > c.height && Math.random() > 0.95) {
+            drops[i] = 0;
+        }
+
+        drops[i]++;
+
     }
 
     window.requestAnimationFrame(draw);
-    
+
 }
 
 draw()
